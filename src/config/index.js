244e0391b7e5793d365e931637bbda3f8080295e
Sérgio Schwartz Varejão <varejaosergio@gmail.com>
2020-09-23 22:15:07 -0700

    heroku deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL = window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: 'https://krystiankymerson.herokuapp.com/';
+: 'https://kkfly.herokuapp.com/';
 
 export default { 
     URL,
